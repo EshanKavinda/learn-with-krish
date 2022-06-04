c3e86604d9854115e8898687b4ef5f4e9dc4554e
EshanKavinda <eshan98kavinda@gmail.com>
2022-06-04 14:02:40 +0530

    execrise 1 modified

--- find_missing_no_in_array/index.js
+++ find_missing_no_in_array/index.js
@@ -1,7 +1,7 @@
 function findMissingInArr(num) {
     var max = Math.max.apply(Math, num);
     var min = Math.min.apply(Math, num);
-    var missingNo;
+    var missingNo = null;
     var arrSize = num.length;
   
     for(let i=min; i<= max; i++) {
@@ -9,8 +9,12 @@ function findMissingInArr(num) {
         missingNo = i;
       }
     }
+
+    if(missingNo==null){
+      missingNo = min-1;
+    }
     return missingNo;
   }
   
-  var numberSeq= [21,25,29,28,22,24,27,26,30];
+  var numberSeq= [21,25,29,28,22,24,27,26,30,23];
   console.log(findMissingInArr(numberSeq));
