852a453fda0a71e174aa35e62cb0ca14baa3c267
EshanKavinda <eshan98kavinda@gmail.com>
2022-06-04 14:35:39 +0530

    execrise 1 modified

--- find_missing_no_in_array/index.js
+++ find_missing_no_in_array/index.js
@@ -1,21 +1,30 @@
-function findMissingInArr(num) {
+function findMissingInArr(start, num) {
     var max = Math.max.apply(Math, num);
-    var min = Math.min.apply(Math, num);
+    var min = start;
     var missingNo = null;
     var arrSize = num.length;
-  
-    for(let i=min; i<= max; i++) {
-      if(!num.includes(i)) { 
-        missingNo = i;
-      }
-    }
 
-    if(missingNo==null){
-      missingNo = min-1;
+    if(num.includes(min)){
+      for(let i=min; i<= max; i++) {
+        if(!num.includes(i)) { 
+          missingNo = i;
+        }
+      }
+      if(missingNo==null){
+        missingNo = max+1;
+      }
+    }else{
+      missingNo = min;
     }
     
     return missingNo;
   }
   
-  var numberSeq= [21,25,29,28,22,24,27,26,30];
-  console.log(findMissingInArr(numberSeq));
+  var start = 21;
+  var numberSeq0= [21,25,29,28,22,24,27,26,30]; // missing value is 23
+  var numberSeq1= [25,29,28,22,24,27,26,30,23]; // missing value is 21 (minimum)
+  var numberSeq2= [21,25,29,28,22,24,27,26,23]; // missing value is 30 (maximum)
+
+  console.log(findMissingInArr(start, numberSeq0));
+  console.log(findMissingInArr(start, numberSeq1));
+  console.log(findMissingInArr(start, numberSeq2));
